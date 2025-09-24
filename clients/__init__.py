7330094547dd1c9151db29d792dcf68169c1a41e
adw777 <aman.dogra@thewasserstoff.com>
2025-09-24 20:41:36 +0530

    catch_cap updates

--- clients/__init__.py
+++
@@ -1,6 +0,0 @@
-from .openai_client import OpenAIClient
-from .gemini_client import GeminiClient
-from .qdrant_client import QdrantClient
-from .mongodb_client import MongoDBClient
-
-__all__ = ['OpenAIClient', 'GeminiClient', 'QdrantClient', 'MongoDBClient']
